231b99a12458ba5831645998e75cf2779aa5a3b4
Geniedevice <startblack7@naver.com>
2026-03-26 17:54:31 +0900

    CombatChange

--- CombatManager.cpp
+++ CombatManager.cpp
@@ -5,9 +5,9 @@
 
 void CombatManager::StartBattle(BaseCharacter& Player, BaseCharacter& Monster)
 {
-    GraphicManager& gm = GraphicManager::GetInstance();
-    gm.ClearLogs();
-    gm.AddLog(Monster.GetNickname() + " Detected");
+    GraphicManager& Gm = GraphicManager::GetInstance();
+    Gm.ClearLogs();
+    Gm.AddLog(Monster.GetNickname() + " Detected");
 
     while (Player.GetHealth() > 0 && Monster.GetHealth() > 0)
     {
@@ -15,9 +15,9 @@ void CombatManager::StartBattle(BaseCharacter& Player, BaseCharacter& Monster)
 
         // --- Player Turn ---
         Monster.SetHealth(Monster.GetHealth() - Player.GetStrength());
-        gm.AddLog(Player.GetNickname() + " Attack ! -" + to_string(Player.GetStrength()));
+        Gm.AddLog(Player.GetNickname() + " Attack ! -" + to_string(Player.GetStrength()));
 
-        gm.GoSpace(0, 30); cout << " [ Enter: Next Turn ]                                ";
+        Gm.GoSpace(0, 30); cout << " [ Enter: Next Turn ]                                ";
         cin.ignore(100, '\n');
         cin.get();
         if (Monster.GetHealth() <= 0) break;
@@ -26,7 +26,7 @@ void CombatManager::StartBattle(BaseCharacter& Player, BaseCharacter& Monster)
 
         // --- Monster Turn ---
         Player.SetHealth(Player.GetHealth() - Monster.GetStrength());
-        gm.AddLog("?? " + Monster.GetNickname() + " Attack ! -" + to_string(Monster.GetStrength()));
+        Gm.AddLog("?? " + Monster.GetNickname() + " Attack ! -" + to_string(Monster.GetStrength()));
 
         //gm.GoSpace(0, 30); cout << " [ Enter: Next Turn ]                                ";
     }
@@ -35,35 +35,35 @@ void CombatManager::StartBattle(BaseCharacter& Player, BaseCharacter& Monster)
 
     if (Player.GetHealth() > 0)
     {
-        gm.AddLog("Victory");
-        gm.AddLog("Reward");
+        Gm.AddLog("Victory");
+        Gm.AddLog("Reward");
         //player.set_gold() += 78;
     }
     else {
-        gm.AddLog("Fail");
+        Gm.AddLog("Fail");
     }
 }
 
 void CombatManager::UpdateBattleUI(BaseCharacter& Player, BaseCharacter& Monster)
 {
-    GraphicManager& gm = GraphicManager::GetInstance();
+    GraphicManager& Gm = GraphicManager::GetInstance();
 
-    gm.DrawLayout();
+    Gm.DrawLayout();
 
-    gm.GoSpace(5, 2);  cout << "[ PLAYER STATUS ]";
-    gm.GoSpace(5, 3);  cout << "NAME : " << Player.GetNickname();
-    gm.GoSpace(5, 4);  cout << "HP   : " << Player.GetHealth() << " / 100";
+    Gm.GoSpace(5, 2);  cout << "[ PLAYER STATUS ]";
+    Gm.GoSpace(5, 3);  cout << "NAME : " << Player.GetNickname();
+    Gm.GoSpace(5, 4);  cout << "HP   : " << Player.GetHealth() << " / 100";
 
-    gm.GoSpace(75, 2);  cout << "[ ENEMY STATUS ]";
-    gm.GoSpace(75, 3);  cout << "NAME : " << Monster.GetNickname();
-    gm.GoSpace(75, 4);  cout << "HP   : " << (Monster.GetHealth() < 0 ? 0 : Monster.GetHealth()) << " / 50"; //Max_Health
+    Gm.GoSpace(75, 2);  cout << "[ ENEMY STATUS ]";
+    Gm.GoSpace(75, 3);  cout << "NAME : " << Monster.GetNickname();
+    Gm.GoSpace(75, 4);  cout << "HP   : " << (Monster.GetHealth() < 0 ? 0 : Monster.GetHealth()) << " / 50"; //Max_Health
 
     //gm.d("SLIME", 40, 4);
 
-    gm.GoSpace(72, 21); cout << "- GOLD: " << Player.GetGold() << "G";
-    gm.GoSpace(72, 22); cout << "- ATK: " << Player.GetStrength();
-    gm.GoSpace(72, 24); cout << "[ Inventory ]";
-    gm.GoSpace(72, 25); cout << " 1. Recovery (HP)";
+    Gm.GoSpace(72, 21); cout << "- GOLD: " << Player.GetGold() << "G";
+    Gm.GoSpace(72, 22); cout << "- ATK: " << Player.GetStrength();
+    Gm.GoSpace(72, 24); cout << "[ Inventory ]";
+    Gm.GoSpace(72, 25); cout << " 1. Recovery (HP)";
 }
 
 void CombatManager::Reward(BaseCharacter& Player)
