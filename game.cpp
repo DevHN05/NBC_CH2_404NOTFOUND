258f7116f0977f72ccf31b3f48b1186d91bf89d6
SeungJae <devhn0@gmail.com>
2026-03-26 09:58:41 +0900

    Add game.cpp, game.h

---
+++ game.cpp
@@ -0,0 +1,36 @@
+﻿#include "game.h"
+#include "character.h"
+
+using namespace std;
+
+int game::get_chapter()
+{
+    return chapter_;
+}
+
+int game::get_stage()
+{
+    return stage_;
+}
+
+void game::set_chapter(int chapter)
+{
+    chapter_ = chapter;
+}
+
+void game::set_stage(int stage)
+{
+    stage_ = stage;
+}
+
+void game::start_game()
+{
+    string nickname_;
+    
+    cout << "Type your nickname: ";
+    cin >> nickname_;
+    
+    character character(nickname_);
+    
+    cout << "Is your nickname " << character.get_nickname() << "?\n";
+}
