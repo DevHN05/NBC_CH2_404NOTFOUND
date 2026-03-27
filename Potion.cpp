706c300f42c46077cd712299f59dad6183f75f2d
osh010420 <osh4306@naver.com>
2026-03-27 14:31:42 +0900

    [성현] 포션

--- Potion.cpp
+++ Potion.cpp
@@ -1,9 +1,13 @@
 #include "Potion.h"
-#include <algorithm>
 
 int Potion::GetCount() const
 {
 	return Count;
+}
+
+int Potion::GetRecovery() const
+{
+	return Recovery;
 }
 
 void Potion::SetCount(int count)
@@ -11,43 +15,3 @@ void Potion::SetCount(int count)
 	Count = count;
 }
 
-void Potion::Use(BaseCharacter& character)
-{
-	if (Count <= 0)
-	{
-		cout << "You have no more " << GetName() << " left!" << endl;
-		return;
-	}
-
-	switch (PotionType)
-	{
-	case EPotionType::Health:
-	{
-		int newHealth = character.GetHealth() + Recovery;
-		character.SetHealth(min(newHealth, character.GetMaxHealth()));
-
-		cout << "You used " << GetName()
-			<< ". Health recovered by " << Recovery
-			<< ". Current health: " << character.GetHealth()
-			<< "/" << character.GetMaxHealth() << "." << endl;
-		break;
-	}
-
-	case EPotionType::Strength:
-	{
-		character.SetStrength(character.GetStrength() + Recovery);
-		cout << "You used " << GetName()
-			<< ". Strength increased by " << Recovery
-			<< ". Current strength: " << character.GetStrength() << "." << endl;
-		break;
-	}
-	
-
-	default:
-		cout << "Unknown potion type." << endl;
-		return;
-	}
-
-	Count--;
-	cout << "Remaining count of " << GetName() << ": " << Count << endl;
-}
