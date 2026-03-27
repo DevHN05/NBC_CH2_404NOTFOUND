273f9a853563b484c304d9ab5c720656768bdb60
DevHN <devhn0@gmail.com>
2026-03-27 13:41:45 +0900

    [승재] Initial commit

---
+++ PlayerManager.cpp
@@ -0,0 +1,84 @@
+﻿#include "PlayerManager.h"
+
+int PlayerManager::GetLevel()
+{
+    return Level;
+}
+
+int PlayerManager::GetExperience()
+{
+    return Experience;
+}
+
+int PlayerManager::GetDexterity()
+{
+    return Dexterity;
+}
+
+int PlayerManager::GetIntelligence()
+{
+    return Intelligence;
+}
+
+int PlayerManager::GetLuck()
+{
+    return Luck;
+}
+
+int PlayerManager::GetCriticalProbability()
+{
+    return CriticalProbability;
+}
+
+int PlayerManager::GetGold()
+{
+    return Gold;
+}
+
+void PlayerManager::SetLevel(int Level)
+{
+    this->Level = Level;
+}
+
+void PlayerManager::SetExperience(int Experience)
+{
+    this->Experience = Experience;
+}
+
+void PlayerManager::SetDexterity(int Dexterity)
+{
+    this->Dexterity = Dexterity;
+}
+
+void PlayerManager::SetIntelligence(int Intelligence)
+{
+    this->Intelligence = Intelligence;
+}
+
+void PlayerManager::SetLuck(int Luck)
+{
+    this->Luck = Luck;
+}
+
+void PlayerManager::SetCriticalProbability(int CriticalProbability)
+{
+    this->CriticalProbability = CriticalProbability;
+}
+
+void PlayerManager::SetGold(int Gold)
+{
+    this->Gold = Gold;
+}
+
+void PlayerManager::PrintPlayerStatus()
+{
+    cout << GetNickname() << "\n";
+    cout << Level << "\n";
+    cout << Experience << "\n";
+    cout << GetHealth() << "\n";
+    cout << GetStrength() << "\n";
+    cout << Dexterity << "\n";
+    cout << Intelligence << "\n";
+    cout << CriticalProbability << "\n";
+    cout << Gold << "\n";
+}
---
+++ PlayerManager.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include "BaseCharacter.h"
+
+class PlayerManager : public BaseCharacter
+{
+public:
+    int GetLevel();
+    int GetExperience();
+    int GetMaxExperience();
+    int GetDexterity();
+    int GetIntelligence();
+    int GetLuck();
+    int GetCriticalProbability();
+    int GetGold();
+
+    void SetLevel(int Level);
+    void SetExperience(int Experience);
+    void setMaxExperience(int Experience);
+    void SetDexterity(int Dexterity);
+    void SetIntelligence(int Intelligence);
+    void SetLuck(int Luck);
+    void SetCriticalProbability(int CriticalProbability);
+    void SetGold(int Gold);
+
+    void PrintPlayerStatus();
+
+private:
+    int Level = 0;
+    int Experience = 0;
+    int MaxExperience = 100;
+    int Dexterity = 0;
+    int Intelligence = 0;
+    int Luck = 0;
+    int CriticalProbability = 0;
+    int Gold = 0;
+};
