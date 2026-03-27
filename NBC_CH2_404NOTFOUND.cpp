8e0d78773ac876d5a5227cd4861961df874b52c4
Your Name <your.email@example.com>
2026-03-27 15:55:19 +0900

    [튜터] 한글 깨져나오는 현상 수정

--- NBC_CH2_404NOTFOUND.cpp
+++ NBC_CH2_404NOTFOUND.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <windows.h>
 #include "GameSystem.h"
 
 using namespace std;
 
 int main()
 {
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
     GameSystem Game;
     Game.StartGame();
     
