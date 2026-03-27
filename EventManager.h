06b23a8cc42c02dabc64ab78a0790876815c1f53
DESKTOP-BVBCMGF\Home <chlalstj24@naver.com>
2026-03-27 20:09:07 +0900

    [민서] EventManager 생성 및 기초 기능 구현

---
+++ EventManager.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include <vector>
+#include "DiceSystem.h"
+#include "PlayerManager.h"
+
+class EventManager {
+public:
+    EventManager(PlayerManager& InPlayer);
+    void TriggerNextEvent();
+
+private:
+    void ShuffleEvents();
+
+    void BattleFirewall(); 		 // 노말 이벤트 #1 vs시스템 보안 봇
+    void BattleMemoryLeak();	 // 노말 이벤트 #2 vs방랑자 :: delete ptr
+	void ChoiceGarbageCollection			 // 전투/보상 이벤트 #1 vs가비지컬렉션(miniboss)
+    void ChestNormal(); 		 // 보상 이벤트 #1 평범한 보물상자
+	void ShopChoiceEvent					 // 상점 이벤트 #1 갈림길
+
+    PlayerManager& Player;
+    DiceSystem Dice;
+    std::vector<int> EventIds;
+    int CurrentEventIndex;
+};
