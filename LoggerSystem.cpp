25d2172f3d097f43148a0c18d6ad5667d2925ea0
KYMMMM01 <dbals498@gmail.com>
2026-03-27 14:13:00 +0900

    [유민] LoggerSystem.cpp 함수 내용 수정

--- LoggerSystem.cpp
+++ LoggerSystem.cpp
@@ -1,92 +1,115 @@
 #include "LoggerSystem.h"
 #include <iostream>
+#include "GraphicManager.h"
 
 //----------------------------전투 관련-----------------------------------
 //몬스터가 나타났을때 로그 출력하는 함수
-void LoggerSystem::LogMonsterAppear(const string& MonsterName) 
+void LoggerSystem::LogMonsterAppear(const string& MonsterName)
 {
-	string Log = "[ DETECTION ] 버그 개체 [ " + MonsterName + " ] 접근 확인. 디버깅 개시.";
-	cout << Log << "\n";
+    GraphicManager& Gm = GraphicManager::GetInstance();
+    string Log = "[ DETECTION ] 버그 개체 [ " + MonsterName + " ] 접근 확인. 디버깅 개시.";
+
+    Gm.AddLog(Log);
 	EventLogs.push_back(Log);
 };
 
 //공격을 했을 때 로그 출력하는 함수
-void LoggerSystem::LogAttack(const string& Attacker, const string& Target, int Damage) 
+void LoggerSystem::LogAttack(const string& Attacker, const string& Target, int Damage)
 {
+    GraphicManager& Gm = GraphicManager::GetInstance();
 	string Log = "[ ATTACK ] " + Attacker + " >> " + Target + " 에 디버깅 실행 / 데미지: " + to_string(Damage);
-	cout << Log << "\n";
+
+    Gm.AddLog( Log );
 	EventLogs.push_back(Log);
 };
 
 //몬스터를 처치했을때 로그 출력하는 함수(해당 몬스터의 처치수를 ++함)
-void LoggerSystem::LogMonsterKill(const string& MonsterName) 
+void LoggerSystem::LogMonsterKill(const string& MonsterName)
 {
+    GraphicManager& Gm = GraphicManager::GetInstance();
 	MonsterKillLogs[MonsterName]++;
 	string Log = "[ DEBUG COMPLETE ] " + MonsterName + " 제거됨. 데이터 수집 중...";
-	cout << Log << "\n";
+
+    Gm.AddLog(Log);
 	EventLogs.push_back(Log);
 };
 
 //보스 페이즈 변화 로그 출력 함수
 void LoggerSystem::LogBossPhaseChange(const string& Nickname, const string& SpecialSkillName, int BoostedStrength)
 {
+    GraphicManager& Gm = GraphicManager::GetInstance();
 	string Log = "[ CRITICAL WARNING ] 버그 개체 [ " + Nickname + " ] 자가 코드 재작성 감지\n"
 		+ "  >> 권한 레벨 상승 / " + SpecialSkillName + " 활성화\n"
 		+ "  >> ATK → " + to_string(BoostedStrength) + "  |  디버깅 난이도 상승";
-	cout << Log << "\n";
+
+    Gm.AddLog(Log);
 	EventLogs.push_back(Log);
 };
 
 //주사위 로그 출력 함수
 void LoggerSystem::LogDiceRoll(int Head, bool IsSucceeded)
 {
+    GraphicManager& Gm = GraphicManager::GetInstance();
 	string Result = IsSucceeded ? "성공!" : "실패...";
 	string Log = "[ DICE ] 주사위 " + to_string(Head) + " → " + Result;
-	cout << Log << "\n";
+
+    Gm.AddLog(Log);
 	EventLogs.push_back(Log);
 }
 //-----------------------------캐릭터 관련--------------------------------------
-void LoggerSystem::LogLevelUp(int NewLevel) //레벨업 했을때 로그 출력하는 함수 
+void LoggerSystem::LogLevelUp(int NewLevel) //레벨업 했을때 로그 출력하는 함수
 {
+    GraphicManager& Gm = GraphicManager::GetInstance();
 	string Log = "[ SYSTEM ] 디버깅 툴 강화 완료 — 권한 레벨 " + to_string(NewLevel) + " 해금";
-	cout << Log << "\n";
+
+    Gm.AddLog(Log);
 	EventLogs.push_back(Log);
 };
 void LoggerSystem::LogPlayerDeath() //플레이어 사망시 로그 출력하는 함수
 {
+    GraphicManager& Gm = GraphicManager::GetInstance();
 	string Log = "[ CONNECTION LOST ] 디버거 강제 종료. 재접속 시도 중...";
-	cout << Log << "\n";
-	EventLogs.push_back(Log); 
+
+    Gm.AddLog(Log);
+	EventLogs.push_back(Log);
 };
 
 //----------------------------------------아이템 관련----------------------------------------
 //아이템 사용시 로그 출력하는 함수(아이템명과 효과)
-void LoggerSystem::LogItemUse(const string& ItemName, const string& Effect) 
+void LoggerSystem::LogItemUse(const string& ItemName, const string& Effect)
 {
+    GraphicManager& Gm = GraphicManager::GetInstance();
 	string Log = "[ TOOL ACTIVATED ] " + ItemName + " 가동 — " + Effect;
-	cout << Log << "\n";
+
+    Gm.AddLog(Log);
 	EventLogs.push_back(Log);
 };
 //-----------------------------------보상 관련-------------------------------------------
 //경험치 획득시 로그 출력하는 함수(얻은량, 현재경험치/레벨업에 필요한 총 경험치)
-void LoggerSystem::LogExpGain(int Amount, int MyExp, int MaxExp) 
+void LoggerSystem::LogExpGain(int Amount, int MyExp, int MaxExp)
 {
 	TotalExp += Amount;
+    GraphicManager& Gm = GraphicManager::GetInstance();
 	string Log = "[ DATA COLLECTED ] " + to_string(Amount) + " 데이터 수집 / 현재: " + to_string(MyExp) + " / " + to_string(MaxExp);
-	EventLogs.push_back(Log);
+
+    Gm.AddLog(Log);
+    EventLogs.push_back(Log);
 };
 
 //골드 획득시 로그 출력하는 함수(얻은량, 현재 골드량)
-void LoggerSystem::LogGoldGain(int Amount) 
+void LoggerSystem::LogGoldGain(int Amount)
 {
 	TotalGold += Amount;
+    GraphicManager& Gm = GraphicManager::GetInstance();
 	string Log = "[ RESOURCE ] " + to_string(Amount) + " 골드 확보 / 누적 골드: " + to_string(TotalGold);
-	EventLogs.push_back(Log);
+
+    Gm.AddLog(Log);
+    EventLogs.push_back(Log);
 };
 
 
 //-----------------------------요약----------------------------------
-//처치한 몬스터와 획득한 총 골드와 경험치를 보여주는 함수 
+//처치한 몬스터와 획득한 총 골드와 경험치를 보여주는 함수
 void LoggerSystem::PrintSummary()
 {
 	cout << "======================================\n";
@@ -94,7 +117,7 @@ void LoggerSystem::PrintSummary()
 	cout << "======================================\n";
 
 	cout << "[ 제거된 버그 개체 ]\n";
-	if (MonsterKillLogs.empty())
+	 if (MonsterKillLogs.empty())
 	{
 		cout << "  제거된 버그 개체 없음\n";
 	}
